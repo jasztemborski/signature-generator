7c4d5d3cc0c2b830c8f77f447c75f4b476bb9b70
Kuba <you@example.com>
2018-04-06 15:07:46 +0200

    test

---
+++ scripts.js
@@ -0,0 +1,29 @@
+(function($) {
+  $(document).ready(function(){
+    var form = $("#form-generator");
+    form.on('submit', function(event){
+      $.each(form.serializeArray(), function(i, field) {
+          var fieldname = field.name + "_field";
+          if(field.value != "") {
+            if(fieldname.indexOf("social") >= 0) {
+              $("#"+fieldname).attr("src", field.value).attr("href", field.value);
+            } else {
+              $("#"+fieldname).text(field.value);
+            }
+          } else {
+            if(fieldname != "optional_text_1_field") {
+              $("#"+fieldname).remove();
+            }
+          }
+      });
+      // if(phone_number_2.value == '') {
+      //   $('.spacer').html('<br/><br/>');
+      // }
+      return false;
+    })
+  });
+  function receivedImage() {
+    $("#employee_avatar").attr("src", fr.result);
+  }
+
+})(jQuery);
